b5c822aa26d3637a00b4093c95647bddb093d53d
zbl14 <ifthereisoneday@gmail.com>
2022-06-24 14:54:42 -0700

    create form for collecting user's name, age, race, gender and residence

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import { Age } from '../src/js/age.js';
+
+$(document).ready(() => {
+  $("form#inputtedDate").submit((event) => {
+    event.preventDefault();
+    const name = $("#inputtedName").val();
+    const ageOnEarth = $("#inputtedAge").val();
+    const gender = $("#inputtedGender").val();
+    const race = $("#inputtedRace").val();
+    const country = $("#inputtedCountry").val();
+    let age = new Age(name, ageOnEarth, gender, race, country);
+    age
+  });
+});
