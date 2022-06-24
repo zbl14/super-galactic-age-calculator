89b578cd10a3326bd5a56d112ce4fc5a6c6db495
zbl14 <ifthereisoneday@gmail.com>
2022-06-24 09:42:04 -0700

    should return age in Mecury years, venus years, mars years and jupiters years, test passed

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -18,17 +18,17 @@ describe('Age', () => {
     expect(reusableAge.country).toEqual("USA");
   });
 
-  test('should retune age in Mercury years', () => {
+  test('should return age in Mercury years', () => {
     reusableAge.galacticAge(); 
-    expect(reusableAge.mercuryY).toEqual(7.2)
+    expect(reusableAge.mercuryY).toEqual(125.00)
   }); 
 
-  test('should retune age in Mercury years, Venus years, Mars years, Jupiter years', () => {
+  test('should return age in Mercury years, Venus years, Mars years and Jupiter years', () => {
     reusableAge.galacticAge(); 
-    expect(reusableAge.mercuryY).toEqual(7.2)
-    expect(reusableAge.venusY).toEqual(18.6)
-    expect(reusableAge.marsY).toEqual(56.4)
-    expect(reusableAge.jupiterY).toEqual(355.8)
+    expect(reusableAge.mercuryY).toEqual(125.00)
+    expect(reusableAge.venusY).toEqual(48.39)
+    expect(reusableAge.marsY).toEqual(15.96)
+    expect(reusableAge.jupiterY).toEqual(2.53)
   }); 
 
 
--- src/js/age.js
+++ src/js/age.js
@@ -8,7 +8,10 @@ export class Age {
   }
 
   galacticAge () {
-    this.mercuryY = parseFloat((this.age * 0.24).toFixed(2));
+    this.mercuryY = parseFloat((this.age / 0.24).toFixed(2));
+    this.venusY = parseFloat((this.age / 0.62).toFixed(2));
+    this.marsY = parseFloat((this.age / 1.88).toFixed(2));
+    this.jupiterY = parseFloat((this.age / 11.86).toFixed(2));
     return this.mercuryY
   }
 }
