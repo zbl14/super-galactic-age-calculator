4419128e98d3dd952a36be2da5e19b96353cb136
zbl14 <ifthereisoneday@gmail.com>
2022-06-24 09:25:36 -0700

    refactory exsiting test with reusableAge, tests passed

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -11,18 +11,16 @@ describe('Age', () => {
   });
 
   test('should return Age object with five properties for name, age, gender, race and country',() => {
-    const age = new Age("Ben", 30, "male", "Asian", "USA")
-    expect(age.name).toEqual("Ben");
-    expect(age.age).toEqual(30);
-    expect(age.gender).toEqual("male");
-    expect(age.race).toEqual("Asian");
-    expect(age.country).toEqual("USA");
+    expect(reusableAge.name).toEqual("Ben");
+    expect(reusableAge.age).toEqual(30);
+    expect(reusableAge.gender).toEqual("male");
+    expect(reusableAge.race).toEqual("Asian");
+    expect(reusableAge.country).toEqual("USA");
   });
 
   test('should retune age in Mercury years', () => {
-    const age = new Age("Ben", 30, "male", "Asian", "USA")
-    age.galacticAge(); 
-    expect(age.mercuryY).toEqual(7.2)
+    reusableAge.galacticAge(); 
+    expect(reusableAge.mercuryY).toEqual(7.2)
   }); 
 
 
