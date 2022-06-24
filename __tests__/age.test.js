3c36c3231246215900069a188d27640dfbe896f7
zbl14 <ifthereisoneday@gmail.com>
2022-06-24 09:22:31 -0700

    add a reusableAge, test passed

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,7 +1,15 @@
 import { Age } from '../src/js/age.js';
 
 describe('Age', () => {
+  let reusableAge;
+  beforeEach(() => {
+    reusableAge = new Age("Ben", 30, "male", "Asian", "USA")
+  }) 
   
+  test('should show how beforeEach() works', () => {
+    console.log(reusableAge);
+  });
+
   test('should return Age object with five properties for name, age, gender, race and country',() => {
     const age = new Age("Ben", 30, "male", "Asian", "USA")
     expect(age.name).toEqual("Ben");
@@ -9,11 +17,13 @@ describe('Age', () => {
     expect(age.gender).toEqual("male");
     expect(age.race).toEqual("Asian");
     expect(age.country).toEqual("USA");
-  })
+  });
 
   test('should retune age in Mercury years', () => {
     const age = new Age("Ben", 30, "male", "Asian", "USA")
     age.galacticAge(); 
     expect(age.mercuryY).toEqual(7.2)
   }); 
+
+
 });
