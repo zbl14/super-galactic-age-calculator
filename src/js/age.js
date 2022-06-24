2672b158c2a2a4a9c5e0266e4cca9936116d3b00
zbl14 <ifthereisoneday@gmail.com>
2022-06-24 10:33:28 -0700

    refactory the code with toFixed and parseFlote to address the desimal issues, test pass

--- src/js/age.js
+++ src/js/age.js
@@ -18,21 +18,21 @@ export class Age {
   yrsleftOnEarth () {
     let yrsleft;
     if (this.gender === "male" && this.race === "Asian" && this.country === "USA") {
-      yrsleft = 85.3 - this.age;
+      yrsleft = parseFloat((85.3 - this.age).toFixed(2));
     } else if (this.gender === "female" && this.race === "Asian" && this.country === "USA") {
-      yrsleft = 89.7 - this.age;
+      yrsleft = parseFloat((89.7 - this.age).toFixed(2));
     } else if (this.gender === "male" && this.race === "Hispanic" && this.country === "USA") {
-      yrsleft = 81.1 - this.age;
+      yrsleft = parseFloat((81.1 - this.age).toFixed(2));
     } else if (this.gender === "female" && this.race === "Hispanic" && this.country === "USA") {
-      yrsleft = 86.3 - this.age;
+      yrsleft = parseFloat((86.3 - this.age).toFixed(2));
     } else if (this.gender === "male" && this.race === "Non-Hispanic White" && this.country === "USA") {
-      yrsleft = 76.5 - this.age;
+      yrsleft = parseFloat((76.5 - this.age).toFixed(2));
     } else if (this.gender === "female" && this.race === "Non-Hispanic White" && this.country === "USA") {
-      yrsleft = 81.2 - this.age;
+      yrsleft = parseFloat((81.2 - this.age).toFixed(2));
     } else if (this.gender === "male" && this.race === "Non-Hispanic African American" && this.country === "USA") {
-      yrsleft = 72.3 - this.age;
+      yrsleft = parseFloat((72.3 - this.age).toFixed(2));
     } else if (this.gender === "female" && this.race === "Non-Hispanic African American" && this.country === "USA") {
-      yrsleft = 78.8 - this.age;
+      yrsleft = parseFloat((78.8 - this.age).toFixed(2));
     }
     return yrsleft;
   }
